c4dfdc76ac97c8084d3ea3b764ac37690d0b73ad
hail0hydra <thehokage0hiddenleaf@gmail.com>
2026-07-09 09:50:02 +0530

    Add POST endpoint and create Schema for posts
    using PyDantic we created a schema for incoming post request data. Noice
    Also created a POST request endpoint ofcourse.

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI() # FastAPI instance
 
+# Post Schema
+class Post(BaseModel): #extends BaseModel, inherits it, etc
+    title:  str
+    content: str
+    published: bool = True # default
+    rating: Optional[int] = None # optional integer value from Typing Library
+
+
+# GET
 @app.get('/')
 async def root():
     return {"message":"Welcome to my APIMon ( > - < )"} # auto convert to JSON
@@ -9,3 +20,12 @@ async def root():
 @app.get('/posts')
 async def getPosts():
     return {"data": "This is your post ⚔️⚔️⚔️ "}
+
+
+# POST
+@app.post('/createpost')
+async def createPost(req: Post): # using the Post pydantic Model/Schema
+    # because of pydantic model being used, it will auto validate the required things mentioned in the model
+    print(req)
+    print(req.model_dump()) # converts to dict, all pydantic Models have this method
+    return {"data": req}
